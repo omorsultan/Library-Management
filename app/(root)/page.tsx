8f2dc6d7ae2020dd31e56caed00fb832ee05729c
omorsultan <omorsultansust@gmail.com>
2026-08-24 14:19:01 +0600

    fix text-color-200

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,11 +3,16 @@ import { Book } from "lucide-react";
 import Image from "next/image";
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constants";
 
 const Home = () => 
     <>
-    <BookOverview />
-    <BookList /> 
+    <BookOverview{... sampleBooks[0]} />
+    <BookList 
+        title = "Latest Books"
+        books = {sampleBooks}
+        containerClassName = "mt-10"
+    /> 
     </>;
 
 
--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const BookOverview = () => {
+const BookOverview = ({title, author, genre, rating, totalCopies, availableCopies, description, color, cover, video, summary}:Book ) => {
   return <section className="book-overview">
     <div className="flex flex-1 flex-col gap-5">
-      <h1>A Long Book Title</h1>
+      <h1>{title}</h1>
+      <div className="book-info">
+        <p>
+          By <span className="font-semibold text-light-200">{author}</span>
+        </p>
+        <p>
+          Category{" "}
+          <span className="font-semibold text-light-200">{genre}</span>
+        </p>
+      </div>
     </div>
   </section>
 };
